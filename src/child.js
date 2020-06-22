82e60ebf8d66c9dd25a9e28df42609eb714f4e4f
Muhammad Madni <44685932+muhammadmadnihaider@users.noreply.github.com>
2020-06-22 22:52:33 +0500

    adding

--- src/child.js
+++ src/child.js
@@ -1,98 +1,115 @@
-import React, { useContext, useState } from 'react';
-import { TransactionContext } from './transContext';
-
+import React, { useContext, useState } from "react";
+import { TransactionContext } from "./transContext";
 
 function Child() {
-    let { transactions, addTransaction } = useContext(TransactionContext);
-    let [newDesc, setDesc] = useState("");
-    let [newAmount, setAmount] = useState(0);
+  let { transactions, addTransaction, deleteTransaction } = useContext(
+    TransactionContext
+  );
+  let [newDesc, setDesc] = useState("");
+  let [newAmount, setAmount] = useState(0);
 
-
-    const handleAddition = (event) => {
-        event.preventDefault();
-        if (Number(newAmount) === 0) {
-            alert("Please enter correct value");
-            return false;
-        }
-        addTransaction({
-            amount: Number(newAmount),
-            desc: newDesc
-        });
-
-        setDesc('');
-        setAmount(0)
+  const handleAddition = (event) => {
+    event.preventDefault();
+    if (Number(newAmount) === 0) {
+      alert("Please enter correct value");
+      return false;
     }
+    addTransaction({
+      amount: Number(newAmount),
+      desc: newDesc,
+    });
 
-    const getIncome = () => {
-        let income = 0;
-        for (var i = 0; i < transactions.length; i++) {
-            if (transactions[i].amount > 0)
-                income = income + transactions[i].amount
-        }
-        return income;
+    setDesc("");
+    setAmount(0);
+  };
+
+  const getIncome = () => {
+    let income = 0;
+    for (var i = 0; i < transactions.length; i++) {
+      if (transactions[i].amount > 0) income = income + transactions[i].amount;
     }
+    return income;
+  };
 
-    const getExpense = () => {
-        let expense = 0;
-        for (var i = 0; i < transactions.length; i++) {
-            if (transactions[i].amount < 0)
-                expense += transactions[i].amount
-        }
-        return expense;
+  const getExpense = () => {
+    let expense = 0;
+    for (var i = 0; i < transactions.length; i++) {
+      if (transactions[i].amount < 0) expense += transactions[i].amount;
     }
+    return expense;
+  };
 
-    return (
-        <div className="container">
-            <h1 className="text-center">Expense Tracker</h1>
+  return (
+    <div className="container">
+      <h1 className="text-center">Expense Tracker</h1>
 
-            <h3>Your Balance <br /> ${getIncome() + getExpense()}</h3>
+      <h3>
+        Your Balance <br /> ${getIncome() + getExpense()}
+      </h3>
 
-            <div className="expense-container">
-                <h3>INCOME <br /> ${getIncome()}</h3>
-                <h3>EXPENSE <br /> ${getExpense()}</h3>
-            </div>
+      <div className="expense-container">
+        <h3>
+          INCOME <br /> ${getIncome()}
+        </h3>
+        <h3>
+          EXPENSE <br /> ${getExpense()}
+        </h3>
+      </div>
 
-            <h3>History</h3>
-            <hr />
+      <h3>History</h3>
+      <hr />
 
-            <ul className="trnsaction-list">
-                {transactions.map((transObj, ind) => {
-                    return (<li key={ind}>
-                        <span>{transObj.desc}</span>
-                        <span>${transObj.amount}</span>
-                    </li>
-                    )
-                })}
+      <ul className="trnsaction-list">
+        {transactions.map((transObj, ind) => {
+          return (
+            <li key={ind}>
+              <span>{transObj.desc}</span>
+              <span>${transObj.amount}</span>
+              <span>
+                <button
+                  style={{ background: "red", color: "white" }}
+                  onClick={() => deleteTransaction(ind)}
+                  class="btn"
+                >
+                  delete
+                </button>
+              </span>
+            </li>
+          );
+        })}
+      </ul>
 
-            </ul>
+      <h3>Add new transaction</h3>
+      <hr />
 
-            <h3>Add new transaction</h3>
-            <hr />
+      <form className="transaction-form" onSubmit={handleAddition}>
+        <label>
+          Enter Description <br />
+          <input
+            type="text"
+            value={newDesc}
+            placeholder="Description"
+            onChange={(ev) => setDesc(ev.target.value)}
+            required
+          />
+        </label>
 
-            <form className="transaction-form" onSubmit={handleAddition}>
-                <label>
-                    Enter Description <br />
-                    <input type="text"
-                        value={newDesc}
-                        placeholder="Description"
-                        onChange={(ev) => setDesc(ev.target.value)}
-                        required />
-                </label>
-
-                <br />
-                <label>
-                    Enter Amount <br />
-                    <input type="number"
-                        value={newAmount}
-                        placeholder="Amount"
-                        onChange={(ev) => setAmount(ev.target.value)}
-                        required />
-                </label>
-                <br />
-                <input type="submit" value="Add Transaction" />
-            </form>
-        </div>
-    );
+        <br />
+        <label>
+          Enter Amount <br />
+          <input
+            type="number"
+            value={newAmount}
+            placeholder="Amount"
+            onChange={(ev) => setAmount(ev.target.value)}
+            required
+          />
+        </label>
+        <br />
+        <input type="submit" value="Add Transaction" />
+      </form>
+    </div>
+  );
 }
 
 export default Child;
--- src/transReducer.js
+++ src/transReducer.js
@@ -1,11 +1,19 @@
-const TransactionReducer =  ((state, action)=>{
-    switch(action.type){
-        case "ADD_TRANSACTION": {
-            return [action.payload , ...state]
-        }
-        default:
-            return state;
+const TransactionReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TRANSACTION": {
+      return [action.payload, ...state];
     }
-})
+    case "DELETE_TRANSACTION": {
+      let getAllData = [...state];
+      let FilteredData = getAllData.filter(
+        (item, index) => index !== action.payload.itemIndex
+      );
+      debugger;
+      return [...FilteredData];
+    }
+    default:
+      return state;
+  }
+};
 
 export default TransactionReducer;
